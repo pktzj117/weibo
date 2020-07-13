5c274e4dab157ac1571b3ed9382c8ca71a40c824
pktzj117 <416335127@qq.com>
2020-07-13 17:03:39 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,13 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+#账号注册
 Route::get('signup','UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+#登录
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+
