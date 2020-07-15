3843903bbf36e8a8ef8f2016da7c9c2c5c0a9675
pktzj117 <416335127@qq.com>
2020-07-15 17:25:29 +0800

    查看用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(user::class)->times(50)->make();
+        User::insert($users->makeVisible(['password', 'remeber_token'])->toArray());
+
+        $user = User::find(1);
+        $user->name = '狗翔';
+        $user->email = '416335127@qq.com';
+        $user->save();
+    }
+}
