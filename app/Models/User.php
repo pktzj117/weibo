3b84b43e22381c5e40b3c7dbbed5f9194b053f26
pktzj117 <416335127@qq.com>
2020-07-21 14:22:28 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,30 @@ class User extends Authenticatable
                     ->orderBy('created_at', 'desc');
     }
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)){
+            $user_id = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
 }
